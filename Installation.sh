8f5aa35d6580adcd099da0704b2357b8f9e4dc75
MCousins-00 <89318805+MCousins-00@users.noreply.github.com>
2021-08-23 14:29:14 -0700

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -11,8 +11,12 @@ fi
 #################################################################
 ### Function to install Docker & Docker Compose
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 sudo apt install curl
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt install docker-ce
+sudo systemctl status docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install nginx
+sudo apt install docker.io
